515e8c9e1bd83a62525a6cfc3e24c498dc6f4680
Miguel Mota <miguelmota2@gmail.com>
2014-10-30 21:19:50 -0700

    update demo

--- example/main.js
+++ example/main.js
@@ -1,11 +1,16 @@
-var fileInput = document.getElementById('file');
-var output = document.getElementById('output');
+var $ = document;
+
+var fileInput = $.getElementById('file');
+var output = $.getElementById('output');
+var text = $.getElementById('text');
 
 fileInput.onchange = function(e) {
   var file = e.currentTarget.files[0];
 
   fileToBase64(file, function(base64) {
     console.log(base64); // iVBORw0KGgoAAAANSUhEUgAAADY...
+
+    text.value = base64;
 
     output.src = ['data:image/png;base64,', base64].join('');
   });
